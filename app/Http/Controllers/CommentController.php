c9baf837892b0dd2cae4c921d7b698618098d9e5
medamineDev <grinemedamine3@gmail.com>
2017-02-28 14:37:39 +0100

    test

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Comment;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Facades\Response;
+use Illuminate\Support\Facades\Input;
+
+class CommentController extends Controller{
+
+	/**
+	 * Send back all comments as JSON
+	 *
+	 * @return Response
+	 */
+	public function index()
+	{
+
+		$comments = Comment::get();
+
+		return Response::json($comments);
+	
+
+	}
+
+	/**
+	 * Store a newly created resource in storage.
+	 *
+	 * @return Response
+	 */
+	public function store()
+	{
+		Comment::create(array(
+			'author' => Input::get('author'),
+			'text' => Input::get('text')
+		));
+
+		return Response::json(array('success' => true));
+	}
+
+	/**
+	 * Return the specified resource using JSON
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function show($id)
+	{
+		return Response::json(Comment::find($id));
+	}
+
+	/**
+	 * Remove the specified resource from storage.
+	 *
+	 * @param  int  $id
+	 * @return Response
+	 */
+	public function destroy($id)
+	{
+		Comment::destroy($id);
+
+		return Response::json(array('success' => true));
+	}
+
+}
---
+++ app/Http/routes.php
@@ -0,0 +1,52 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register all of the routes for an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+// =============================================
+// HOME PAGE ===================================
+// =============================================
+Route::get('/', function()
+{
+    return View::make('index');
+});
+
+Route::get('/home', function()
+{
+    return View::make('welcome');
+});
+
+
+// =============================================
+// API ROUTES ==================================
+// =============================================
+Route::group(array('prefix' => 'api'), function() {
+
+    // since we will be using this just for CRUD, we won't need create and edit
+    // Angular will handle both of those forms
+    // this ensures that a user can't access api/create or api/edit when there's nothing there
+    Route::resource('comments', 'CommentController',
+        array('except' => array('create', 'edit', 'update')));
+});
+
+// =============================================
+// CATCH ALL ROUTE =============================
+// =============================================
+// all routes that are not home or api will be redirected to the frontend
+// this allows angular to route them
+/*App::missing(function($exception)
+{
+    return View::make('index');
+});*/
+
+/*Route::any('{catchall}', function() {
+    return View::make('index');
+})->where('catchall', '.*');*/
